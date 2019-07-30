62d0610a0cb2e4bf9246ef4e1ea7d947af990fb2
Artem Bazunts <bazunc@gmail.com>
2019-07-30 22:01:55 +0500

    hbvv

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import * as axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [users, setUsers] = useState([]);
+  const [value, setValue] = useState([]);
+
+  const getUsers = () => {
+
+    axios.get('http://localhost:7542/users').then(res => {
+      debugger
+      setUsers(res.data);
+    })
+  };
+
+  useEffect(()=> {
+    getUsers();
+  }, []);
+
+  let createUser = (e) => {
+    axios.post('http://localhost:7542/users').then( res => {
+      getUsers();
+    })
+  };
+
+  let onChange = (e) => {
+      setValue(e.currentTarget.value)
+  };
+
+  return <div>
+    <input onChange={onChange}/>
+    <button onClick={createUser}>Create User</button>
+      {users.map(u => <div>{u.name}</div>)}
     </div>
-  );
 }
 
 export default App;
